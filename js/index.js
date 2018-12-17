e709091a0f0aa2841917c059d2cc6d588ffda9b7
Mark Sleator <mark_sleator@hotmail.com>
2018-12-17 16:52:04 +0000

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 const redacted = document.querySelectorAll('.redacted');
 
+
+
 /*---
 redacted.forEach((el, i) => {
     el.style.transitionDelay = `${i * 50}ms`
 }); ---*/
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
